ff4e8b3bb022d5b65e72e4020f53f94de9220fc4
vinodkumarv <vinodkumar.vasudevan@gmail.com>
2018-12-10 21:45:26 +0800

    Removed div

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,11 +5,11 @@ let quizData = require ('./quiz_data.json');
 class Quiz extends Component {
   constructor(props) {
     super(props)
-    this.state = {quiz_position:1}
+    this.state = { quiz_position:1 }
   }
   render() {
     return (
-      <div class="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+      <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
     )
   }
 
