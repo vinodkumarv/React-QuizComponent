7df02abd40b8fd807345de213cc75a4ac39dffb7
vinodkumarv <vinodkumar.vasudevan@gmail.com>
2018-12-10 21:43:45 +0800

    Removed div

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,7 @@ class Quiz extends Component {
   }
   render() {
     return (
-      <div><div class="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
+      <div class="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
     )
   }
 
